0bd3e24211215a2817d78dae8f5b129076aba920
Cathy Cai <cathycai626@gmail.com>
2017-12-05 12:47:24 -0500

    Update Monster.java

--- v2/Monster.java
+++ v2/Monster.java
@@ -1,33 +1,17 @@
-public class Monster {
+public class Monster extends Character{
     
-    private int health;
-    private int strength;
-    private int defense;
-    private double attackrating;
-    
-    public Monster (){
-        health = 125;
+    public Monster() {
+        hp = 125;
         strength = (int) (Math.random() * 45 + 20);
         defense = 40;
         attackrating = 0.4;
     }
-    
-    public boolean isAlive() {
-        return health > 0;
-    }
-    
-    public int getDefense() {
-        return defense;
-    }
-    
-    public void lowerHP(int dec) {
-        health -= dec;
-    }
-    
-    public int attack(Protagonist charac) {
-	int damage = (int) (strength * attackrating - charac.getDefense());
-	charac.lowerHP(damage);
-	return damage;
+
+    public Monster(int hpIn, int strIn, int defIn, int attackIn) {
+	hp = hpIn;
+	strength = strIn;
+	defense = defIn;
+	attackrating = attackIn;
     }
     
 }
