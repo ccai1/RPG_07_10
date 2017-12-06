fe8d4c65c4cd5dd6125beba4676a6f4e270399a9
Alexander Radu <34072014+aradu01@users.noreply.github.com>
2017-12-05 22:22:21 -0500

    Create Cyclops.java

---
+++ v2/Cyclops.java
@@ -0,0 +1,13 @@
+public class Cyclops extends Protagonist {
+
+    public Cyclops(String nameIn) {
+	    super(nameIn);
+      hp = 250;
+	    attackrating = 0.8;
+    }
+
+    public Cyclops(String nameIn, int hpIn, int strIn, int defIn, int attackIn) {
+        super(nameIn, hpIn, strIn, defIn, attackIn);
+    }
+
+}
